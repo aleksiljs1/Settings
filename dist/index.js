73db006926bb246f7141f71e391ad6233fe31c83
aleksiljs1 <106152516+aleksiljs1@users.noreply.github.com>
2025-02-11 10:41:46 +0100

    Delete dist directory

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const settings_manager_1 = require("./core/settings-manager");
-const settings = settings_manager_1.SettingsManager.getInstance();
-settings.audio.setVolume(70);
-settings.audio.getVolume();
-console.log(`the volume currently is ${settings.audio.getVolume()}`);
-settings.gameplay.setDifficulty("hard");
-console.log(`${settings.gameplay.getDifficulty()}`);
